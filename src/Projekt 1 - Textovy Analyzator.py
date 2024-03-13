d30875644acd331e4f246ad9d8e3d4b466ae9d55
Ellaniuss <heczko.david@gmail.com>
2024-03-13 11:18:25 +0100

    Added # comments to the code

--- src/Projekt 1 - Textovy Analyzator.py
+++ src/Projekt 1 - Textovy Analyzator.py
@@ -34,13 +34,15 @@ email: heczko.david@gmail.com
 discord: ellaniuss
 
 """
+# Creation of spacer for visual separation of code pieces of final print
 spacer = ('-' * 42)
+# Created directory with usernames and passwored that are allowed. Added input commands for username and password
 registered_users = {'bob': '123', 'ann': 'pass123', 'mike': 'password123', 'liz': 'pass123'}
 username = str(input("Username: "))
 password = str(input("Password: "))
 
 print(spacer)
-
+'#Created check of username and password validity'
 logged_in = False
 if username in registered_users:
     if registered_users.get(username) == password:
@@ -51,14 +53,14 @@ else:
     logged_in = False
     exit()
 print(spacer)
-
+# created empty variables for text analysis
 word_count = 0
 word_istitle = 0
 word_isupper = 0
 word_islower = 0
 number_count = 0
 total_sum = 0
-
+# created input request to chose text and validity check of value inputed. Only possible values are numbers 1,2,3
 text_select = input('Please choose text for analysis (1 - 3): ')
 print(spacer)
 if not text_select.isdigit():
@@ -75,12 +77,12 @@ else:
 
 print(spacer)
 
-choice = texts[(text_select - 1)]
-sentence = choice.split()
+choice = texts[(text_select - 1)]  # created variable "choice" to convert input number to index
+sentence = choice.split()  # created list of each word in text on index selected by input through "choice" variable
 
-for word in sentence:
+for word in sentence:  # created loops to check number of words in chosen text
     word_count = word_count + 1
-for word in sentence:
+for word in sentence:  # created loop to analyse text
     if word.istitle():
         word_istitle = word_istitle + 1
     elif word.isupper():
@@ -92,6 +94,7 @@ for word in sentence:
         total_sum = total_sum + int(word)
 
 
+
 print(
     f'There are {word_count} words in the selected text.\n'
     f'There are {word_istitle} titlecase words. \n'
@@ -101,20 +104,21 @@ print(
     f'The sum of all the numbers is {total_sum}'
     )
 print(spacer)
-
+# created dictionary for lenghts of words
 len_count = dict()
-
+# created for loop to save lenghts to the created dictionary
 for word in sentence:
     word_len = len(word)
     if word_len not in len_count:
         len_count[word_len] = 1
     else:
         len_count[word_len] += 1
-
+# print of simple graph header
 print(
     f'{"LEN":<3}| {"OCCURENCES":<20}| {"Nr.":<5}'
     f'\n'
     f'{spacer:}'
     )
+# loop for print of simple graph using found data
 for ln, occur in len_count.items():
     print(f'{ln:<3}| {"*" * occur:<20}| {occur:<5}')
