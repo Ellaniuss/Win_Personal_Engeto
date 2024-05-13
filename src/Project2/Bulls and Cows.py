da6392406f21ecd9bbac6e6c91f45b7416bfe00c
Ellaniuss <162615656+Ellaniuss@users.noreply.github.com>
2024-05-13 13:04:24 +0200

    Re-creation of original script
    Original scrip was re-written and game was re-created by using def and creating specific function.
    
    Original function for generating random 4-digit number stayed same.
    
    Following functions were created:
    - input_check
    - guess_evaluation
    - tries_counter
    - end_game
    - main

--- src/Project2/Bulls and Cows.py
+++ src/Project2/Bulls and Cows.py
@@ -18,13 +18,113 @@ def random_generator():
             gen_number += digit
     return gen_number
 
-game_num = random_generator()
+def input_check(guess):
+  """
+  Function check if the input is 4-digit number.
 
-spacer = ("-" * 40)
-bulls = 0
-cows = 0
-# Uvodni text
-print(
+  Arguments:
+    guess(str): The input string made by player that needs to be checked.
+
+  Returns:
+    bool: True if the input is 4-digit unique number, False if not.
+
+  The function check whether there are other characters than digits, whether it contains exactly 4-digits, whether number doesn't beggin with 0 and whether there are no duplicated characters.
+  If any of these conditions are not met, errors are appended to the list of errors.
+  If errors occurs, they are specifically printed and function retunrs False. Otherwise it returns True.
+
+  """
+
+  errors = []
+
+  error_types = [
+      (not guess.isdigit(), "Not a number."),
+      (len(guess) != 4, "More or less than 4 characters."),
+      (guess[0] == '0', "Beggins with 0."),
+      (len(guess) != len(set(guess)), "Duplicate characters.")
+    ]
+
+  for error, message in error_types:
+    if error:
+      errors.append(message)
+
+  if errors:
+    print(f"An error or errors occured while entering number. Please see below list with details:")
+    print("\n".join(errors))
+    return False
+  else:
+    return True
+
+
+def guess_evaluation(game_num, guess):
+  """
+  Function evalueates player's guess with generated random number.
+
+  Arguments:
+    game_num (str): Randomly generated number by the game.
+    guess(str): The number guessed by the player
+
+  Returns:
+    tuple: Tuple containing the count of bulls and cows.
+
+  This function evaluates number added by player agains generated number by the game.
+  It returns number of bulls for all digits that are correct and on same possition, and cows for all digits that are correct but on different possition.
+  """
+
+  bulls = 0
+  cows = 0
+
+  for dig in range(len(game_num)):
+    if guess[dig] in game_num:
+      if guess[dig] == game_num[dig]:
+        bulls += 1
+      else:
+        cows += 1
+  return bulls, cows
+
+def tries_counter(tries):
+  """
+  Function created to count players tries needed to guess generated number by the game.
+
+  Arguments:
+    tries(int): Current number of tries.
+
+  Retunrs:
+    int: Updated count of tries by 1.
+  """
+  tries += 1
+  return tries
+
+
+def end_game(bulls):
+  """
+  Function evaluates when game ends.
+
+  Arguments:
+    bulls(int): Number of bulls achieved in the game.
+
+  Returns:
+    bool: True if the count of bulls is 4, False if not.
+  """
+  if bulls == 4:
+    return True
+  else:
+    return False
+
+def main():
+  """
+  Executes the Bulls and Cows game.
+
+  This is the main function of the program that serves as entry point for the game.
+  It generates random 4-digit number using random_generator function
+  and prompts user to input guesses until the correct number is wholy guessed,
+  or the player decides to end the game.
+  For each guess, it check validity of input using the input_check function.
+  If the input is correct, it evaluates entered number using guess_evaluation function and returns and determine number of bulls and cows.
+  """
+  game_num = random_generator()
+  spacer = ("-" * 40)
+
+  print(
     "Greetings fellow gamer! \n" +
     spacer + "\n" +
     "I have generated a random 4-digit number for you. \n" +
@@ -33,51 +133,23 @@ print(
     "In case you would like to finish the game please enter 'end' \n" +
     "\n" + "Can you guess generated number ? \n" + spacer
     )
+  
+  tries = 0
 
-game_in_progress = True  # promenna pro urceni prubehu hry
-tries = 0  # promenna pro urceni poctu pokusu
-
-# Hlavni cast kodu pri keterem se vyhodnocuje podobnost zadaneho cisla s cislem generovanym
-while game_in_progress:
-    bulls = 0
-    cows = 0
-
-    guess = input("Enter guessed number or type 'end': ").lower()
-
-    if bulls == 4:
-        game_in_progress = False
-        print("You have guessed correct number and won the game!")
-    elif guess == "end":
-        game_in_progress = False
-        print("Exiting game.")
-    elif not guess.isdigit():
-        print("You have to enter number!")
-        continue
-    elif len(guess) != 4:
-        print("Entered number has to have 4 digits!") 
-        continue
-    elif guess[0] == "0":
-        print("Entered number cannot beggin with 0!")
-        continue
-    elif len(guess) != len(set(guess)):
-        print("The input number contains duplicate values!")
-        continue
+  while True:
+    guess = input("Enter number: ")
+    if not guess == "end":
+      if input_check(guess):
+        tries = tries_counter(tries)
+        bulls, cows = guess_evaluation(game_num, guess)
+        if not end_game(bulls):
+          print(f"Bull: {bulls}" if bulls <= 1 else f"Bulls: {bulls}", f"Cow: {cows}" if cows <= 1 else f"Cows: {cows}")
+        else:
+          print(f"You have guessed it right! It took you {tries} tries.")
+          break
     else:
-        for dig in range(len(game_num)):  # for loop ktery overuje jednotlive znaky na indexech
-            if guess[dig] in game_num:  # overeni zda-li je cislice ve vygenerovanem cisle
-                if guess[dig] == game_num[dig]:  # overeni zda je cislice na stejnem indexu
-                    bulls += 1
-                else:
-                    cows += 1
-        # cast kodu ktery zajistuje mnozna a jednotna cisla
-        if bulls <= 1 and cows <= 1:
-            print(f"Bull: {bulls}, Cow: {cows} \n")
-        else:    
-            print(f"Bulls: {bulls}, Cows: {cows} \n")
-        
-        tries += 1
-        if not bulls < 4:
-            print(f"You have guessed the correct number in {tries} tries. You Win!")
-            game_in_progress = False
-            
-            
+      print("You have ended the game!")
+      break
+
+if __name__ == __main__:
+    main()
