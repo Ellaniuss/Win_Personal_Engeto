7cdd38383b3a29422bf161feb5d5d62e65af989d
Ellaniuss <heczko.david@gmail.com>
2024-12-21 12:56:08 +0100

    Created second script with table showing data about eropean countries, their GDP and GINI between the years 2006 and 20018.

---
+++ src/Project4/Scripts/Secondary_table.sql
@@ -0,0 +1,40 @@
+/*
+ * 1. Created view for relevant data.
+ */
+SELECT
+	e.`year`,
+	e.country,
+	e.population,
+	round(e.GDP) AS GDP,
+	e.gini
+FROM economies e
+LEFT JOIN countries c
+	ON e.country = c.country
+WHERE
+	c.continent = 'Europe'
+	AND
+	e.`year` BETWEEN 2006 AND 2018
+ORDER BY 
+	e.country,
+	e.`year`
+
+/*
+ * 2. Created table from the selection
+ */
+CREATE TABLE t_david_heczko_project_SQL_secondary_final AS 
+	SELECT
+		e.`year`,
+		e.country,
+		e.population,
+		round(e.GDP) AS GDP,
+		e.gini
+	FROM economies e
+	LEFT JOIN countries c
+		ON e.country = c.country
+	WHERE
+		c.continent = 'Europe'
+		AND
+		e.`year` BETWEEN 2006 AND 2018
+	ORDER BY 
+		e.country,
+		e.`year`
